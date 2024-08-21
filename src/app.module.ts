317b020193e302a2eef0ce7c2c2914beb4bbba55
Pasupathi-M <pasupathi.m@kprinfosolution.com>
2024-08-21 18:09:49 +0530

    main file setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path'
+
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    // Config
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(__dirname, '..', `.env.${process.env.NODE_ENV}`)
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { NestExpressApplication } from '@nestjs/platform-express'
+import * as env from 'dotenv'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // load env
+  env?.config()
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['error', 'debug', 'fatal', 'log', 'verbose', 'warn']
+  });
+
+  // cors
+  app.enableCors({
+    origin: "*"
+  })
+
+  // enable swagger docs
+  const swaggerOpts: any = new DocumentBuilder().setTitle('Api Collection').setDescription('').setVersion('0.0.0')
+  const apiDocument = SwaggerModule.createDocument(app, swaggerOpts)
+  SwaggerModule.setup('docs', app, apiDocument)
+
+  const PORT = process.env.PORT || 2200
+  console.log("PORT >>>>>", PORT)
+  await app.listen(PORT);
 }
 bootstrap();
